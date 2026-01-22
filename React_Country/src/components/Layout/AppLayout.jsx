9a26db8d9396d38a00cb6bf8e8044438d83c0278
patelronit21 <ronakpatel2184@gmail.com>
2026-01-22 13:41:30 +0530

    ReactCountry

---
+++ React_Country/src/components/Layout/AppLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import {Header} from '../UI/Header'
+import {Footer} from '../UI/Footer'
+
+export const AppLayout = () => {
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+
---
+++ React_Country/src/components/UI/Loder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../App.css'
+
+ export const Loder = () => {
+  return (
+    <div className='container loader-section'>
+      <div className='loader'></div>
+    </div>
+  )
+}
+
+ 
